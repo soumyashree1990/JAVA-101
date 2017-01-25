60a1fb8e79c68d79dad02a9f1eeb63ed531294e8
soumyashree1990 <soumyashree1990@gmail.com>
2017-01-25 22:30:44 +0530

    1st commit

---
+++ LearnJAVA/main/java/in/co/india/odisha/learnjava/Array.java
@@ -0,0 +1,10 @@
+package in.co.india.odisha.learnjava;
+
+/**
+ * Created by personal on 25-01-2017.
+ */
+public class Array {
+    public static void main(String[] args) {
+        System.out.println("yippeee");
+    }
+}
