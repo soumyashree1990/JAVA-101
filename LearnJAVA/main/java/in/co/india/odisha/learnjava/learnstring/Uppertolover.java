d437779bb6f9875faaa8048bf75b35283620aca9
soumyashree1990 <soumyashree1990@gmail.com>
2017-01-31 00:19:40 +0530

    Upper to lower over

---
+++ LearnJAVA/main/java/in/co/india/odisha/learnjava/learnstring/Uppertolover.java
@@ -0,0 +1,12 @@
+package in.co.india.odisha.learnjava.learnstring;
+
+/**
+ * Created by personal on 31-01-2017.
+ */
+public class Uppertolover {
+    public static void main(String[] args) {
+        String a=new String("GOOD ");
+        String b=a.toLowerCase();
+        System.out.println(b);
+    }
+}
