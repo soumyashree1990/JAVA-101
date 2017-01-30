458306ad90c8d04dec5ce285427132fad9b0666c
soumyashree1990 <soumyashree1990@gmail.com>
2017-01-31 00:19:18 +0530

    Trim over

---
+++ LearnJAVA/main/java/in/co/india/odisha/learnjava/learnstring/TrimString.java
@@ -0,0 +1,15 @@
+package in.co.india.odisha.learnjava.learnstring;
+
+/**
+ * Created by personal on 31-01-2017.
+ */
+
+import java.io.*;
+
+public class TrimString {
+    public static void main(String[] args) {
+        String word=("purple is my favorite colour ");
+        String word1=word.trim();
+        System.out.println("Trimmed word : "+word1);
+    }
+}
