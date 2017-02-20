63402087a5fea1c11c2301526f70450f074b7e0b
Soumya Pattanaik <SOUMYA1@discovery.co.za>
2017-02-20 16:09:26 +0200

    Removed unnecessary files

---
+++ LearnJAVA/JAXWSHelloWorld/src/test/java/in/co/learnjava/jaxwshelloworld/client/GetEmployeeClient.java
@@ -0,0 +1,26 @@
+package in.co.learnjava.jaxwshelloworld.client;
+
+import in.co.learnjava.jaxwshelloworld.model.Counter;
+import in.co.learnjava.jaxwshelloworld.model.Employee;
+import in.co.learnjava.jaxwshelloworld.service.GetEmployeeService;
+import sun.java2d.SurfaceDataProxy;
+
+import javax.xml.namespace.QName;
+import javax.xml.ws.Service;
+import java.net.URL;
+
+/**
+ * Created by soumya1 on 20/02/2017.
+ */
+public class GetEmployeeClient {
+    public static void main(String[] args) {
+        /*Counter counter = new Counter();
+        counter.setId(5);
+
+        GetEmployeeService getEmployeeService = new GetEmployeeService();
+        Employee employee = (Employee) getEmployeeService.getEmployee(counter);
+        System.out.println(employee.getAddress());*/
+
+
+    }
+}
